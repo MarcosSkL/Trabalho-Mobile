0ee812ba7d2d4ad20d2674e5ee45f333e0246703
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-04-05 00:25:55 -0300

    Mudança do Ionic para React Native.

---
+++ corte_no_app/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+	return (
+    	<View style={styles.container}>
+      		<Text style={estilo.header}>Esse é a nossa página</Text>
+      		<StatusBar style="auto" />
+    	</View>
+  );
+}
+
+const styles = StyleSheet.create({
+	container: {
+    	flex: 1,
+    	backgroundColor: '#fff',
+    	alignItems: 'center',
+    	justifyContent: 'center',
+  },
+});
+
+const estilo = StyleSheet.create({
+	header: {
+		flex: 1,
+		backgroundColor:'orange',
+		alignContent: 'center',
+}})
---
+++ corte_no_app/components/entrar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Header = () =>{
+    return(
+        <View>
+            <Text>
+                esse é a tela inicial.
+            </Text>
+        </View>
+)}
+
+const estilo = StyleSheet.create({
+
+}
+)
