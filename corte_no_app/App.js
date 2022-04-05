111040d30ada5e377bbfec1e2c834199402fe7b6
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-04-05 00:45:54 -0300

    Adição de ESlint ao código

--- corte_no_app/App.js
+++ corte_no_app/App.js
@@ -1,27 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
 	return (
-    	<View style={styles.container}>
-      		<Text style={estilo.header}>Esse é a nossa página</Text>
-      		<StatusBar style="auto" />
-    	</View>
-  );
+		<View style={styles.container}>
+			<Text style={estilo.header}>Esse é a nossa página</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-    	flex: 1,
-    	backgroundColor: '#fff',
-    	alignItems: 'center',
-    	justifyContent: 'center',
-  },
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
 
 const estilo = StyleSheet.create({
 	header: {
 		flex: 1,
-		backgroundColor:'orange',
-		alignContent: 'center',
-}})
+		backgroundColor:"orange",
+		alignContent: "center",
+	}});
--- corte_no_app/components/entrar.jsx
+++ corte_no_app/components/entrar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+// Esse é o cabeçalho da página
 const Header = () =>{
-    return(
-        <View>
-            <Text>
+	return(
+		<View>
+			<Text style={estilo.header}>
                 esse é a tela inicial.
-            </Text>
-        </View>
-)}
-
+			</Text>
+		</View>
+	);};
+// Esse é o estilo de texto
 const estilo = StyleSheet.create({
+	header: {
+		flex: 1,
+		backgroundColor: "black",
+		fontFamily: "Roboto"
 
+	}
 }
-)
+);
